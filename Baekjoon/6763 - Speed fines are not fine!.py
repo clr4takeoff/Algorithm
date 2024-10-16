0268c0bb74ee1b7e5e2125ecd60774058737c32b
clr4takeoff <132669940+clr4takeoff@users.noreply.github.com>
2024-10-17 00:50:50 +0900

    6763 - Speed fines are not fine!

---
+++ Baekjoon/6763 - Speed fines are not fine!.py
@@ -0,0 +1,15 @@
+#B4_6763_Speed fines are not fine!
+
+a=int(input())
+b=int(input())
+
+if a>=b:
+  print("Congratulations, you are within the speed limit!")
+else:
+  if b-a<=20:
+    fine=100
+  elif b-a<=30:
+    fine=270
+  else:
+    fine=500
+  print(f"You are speeding and your fine is ${fine}.")

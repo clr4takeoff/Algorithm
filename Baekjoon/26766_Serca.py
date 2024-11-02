443c1191219c8a59ea1564865aee775c83ef0a8c
clr4takeoff <132669940+clr4takeoff@users.noreply.github.com>
2024-11-02 23:54:05 +0900

    26766_Serca

---
+++ Baekjoon/26766_Serca.py
@@ -0,0 +1,12 @@
+#B5_26766_Serca
+
+for _ in range(int(input())):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")

119684166967dc4ba0481e9d1f1ada54e036dd67
clr4takeoff <132669940+clr4takeoff@users.noreply.github.com>
2024-11-10 00:05:03 +0900

    31429 - SUAPC 2023 Summer

---
+++ Baekjoon/31429 - SUAPC 2023 Summer.py
@@ -0,0 +1,32 @@
+#B5_31429_SUAPC 2023 Summer
+
+import sys
+
+def main():
+    n = int(input().strip())
+
+    if n == 1:
+        print("12 1600")
+    elif n == 2:
+        print("11 894")
+    elif n == 3:
+        print("11 1327")
+    elif n == 4:
+        print("10 1311")
+    elif n == 5:
+        print("9 1004")
+    elif n == 6:
+        print("9 1178")
+    elif n == 7:
+        print("9 1357")
+    elif n == 8:
+        print("8 837")
+    elif n == 9:
+        print("7 1055")
+    elif n == 10:
+        print("6 556")
+    elif n == 11:
+        print("6 773")
+
+if __name__ == "__main__":
+    main()

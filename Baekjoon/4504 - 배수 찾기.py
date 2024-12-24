9e8116a4a194015f25d8f07c58ba24e7a78f85a2
clr4takeoff <132669940+clr4takeoff@users.noreply.github.com>
2024-12-24 17:50:59 +0900

    4504 - 배수 찾기

---
+++ Baekjoon/4504 - 배수 찾기.py
@@ -0,0 +1,14 @@
+#B3_4504_배수 찾기
+
+n=int(input())
+  
+while True:
+  a=int(input())
+
+  if a==0:
+    break
+  
+  if a%n==0:
+    print(f"{a} is a multiple of {n}.")
+  else:
+    print(f"{a} is NOT a multiple of {n}.")

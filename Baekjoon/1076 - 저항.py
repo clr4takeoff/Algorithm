168703db87be4e30ca19255f68de27e3f4c3a5d4
clr4takeoff <132669940+clr4takeoff@users.noreply.github.com>
2025-01-06 16:53:32 +0900

    1076 - 저항

---
+++ Baekjoon/1076 - 저항.py
@@ -0,0 +1,5 @@
+#B2_1076_저항
+
+c={"black":[0,1],"brown":[1,10],"red":[2,100],"orange":[3,1000],"yellow":[4,10000],"green":[5,100000],"blue":[6,1000000],"violet":[7,10000000],"grey":[8,100000000],"white":[9,1000000000]}
+a,b,d=input(),input(),input()
+print(int(str(c[a][0])+str(c[b][0]))*c[d][1])
